d431517d71de258e12c875470600c8a7058f75d3
Matt Williamson <mathewbwilliamson@gmail.com>
2021-01-31 21:56:34 -0500

    Refactored and added single item getter

---
+++ src/Services/BoardGameGeekService.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { json } from 'body-parser';
+import parser, { X2jOptions } from 'fast-xml-parser';
+import he from 'he';
+
+const xmlOptions: Partial<X2jOptions> = {
+    attributeNamePrefix: '@_',
+    attrNodeName: 'attr', // default is 'false'
+    textNodeName: '#text',
+    ignoreAttributes: false,
+    ignoreNameSpace: false,
+    allowBooleanAttributes: false,
+    parseNodeValue: true,
+    parseAttributeValue: true,
+    trimValues: true,
+    cdataTagName: '__cdata', // default is 'false'
+    cdataPositionChar: '\\c',
+    parseTrueNumberOnly: false,
+    arrayMode: false, // "strict"
+    attrValueProcessor: (val, attrName) => he.decode(val, { isAttributeValue: true }), // default is a=>a
+    tagValueProcessor: (val, tagName) => he.decode(val), // default is a=>a
+    stopNodes: ['parse-me-as-string'],
+};
+
+const parseXML = (response: any) => {
+    const jsonCollection = parser.parse(response, xmlOptions) as any; // [matt] TYPE THIS
+
+    if (!!jsonCollection?.errors?.error) {
+        console.log('\x1b[44m%s \x1b[0m', '[matt] ERROR ');
+
+        throw new Error(jsonCollection?.errors?.error?.message);
+    }
+
+    if (jsonCollection && !!jsonCollection?.message) {
+        console.log('\x1b[44m%s \x1b[0m', '[matt] MESSAGE');
+
+        // [matt] Need to do retries for this. Maybe a redis job??
+        return new Error('They are timing us out');
+    }
+
+    return jsonCollection;
+};
+
+export const getCollectionFromBGG = async (username: string) => {
+    try {
+        const response = (
+            await axios.get<string>(
+                `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&version=1&stats=1&own=1`,
+            )
+        ).data;
+
+        return parseXML(response);
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+export const getSingleBoardGameFromBGG = async (gameIds: string[]) => {
+    const idsToSend = gameIds.join(',');
+
+    try {
+        const response = (
+            await axios.get<string>(`https://www.boardgamegeek.com/xmlapi2/thing?id=${idsToSend}&videos=1&stats=1`)
+        ).data;
+
+        return parseXML(response);
+    } catch (err) {
+        throw new Error(err);
+    }
+};
--- src/Services/CollectionService.ts
+++ src/Services/CollectionService.ts
@@ -1,41 +1,10 @@
-import axios from 'axios';
 import parser, { X2jOptions } from 'fast-xml-parser';
 import he from 'he';
 import { BoardGameItem } from '../../types/BoardGameItems';
 import { RawBoardGameItem } from '../../types/RawBoardGameItems';
 import { NewBoardGameItemModel } from '../Models/BoardGameItem.entity';
 import { collectionRepo } from '../../index';
-
-const xmlOptions: Partial<X2jOptions> = {
-    attributeNamePrefix: '@_',
-    attrNodeName: 'attr', // default is 'false'
-    textNodeName: '#text',
-    ignoreAttributes: false,
-    ignoreNameSpace: false,
-    allowBooleanAttributes: false,
-    parseNodeValue: true,
-    parseAttributeValue: true,
-    trimValues: true,
-    cdataTagName: '__cdata', // default is 'false'
-    cdataPositionChar: '\\c',
-    parseTrueNumberOnly: false,
-    arrayMode: false, // "strict"
-    attrValueProcessor: (val, attrName) => he.decode(val, { isAttributeValue: true }), // default is a=>a
-    tagValueProcessor: (val, tagName) => he.decode(val), // default is a=>a
-    stopNodes: ['parse-me-as-string'],
-};
-
-const getCollectionFromBGG = async (username: string) => {
-    try {
-        return (
-            await axios.get<string>(
-                `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&version=1&stats=1&own=1`,
-            )
-        ).data;
-    } catch (err) {
-        throw new Error(err);
-    }
-};
+import { getCollectionFromBGG, getSingleBoardGameFromBGG } from './BoardGameGeekService';
 
 const parseBoardgameItem = (item: RawBoardGameItem) => {
     return {
@@ -79,68 +48,57 @@ const parseBoardgameItem = (item: RawBoardGameItem) => {
     } as BoardGameItem;
 };
 
+const transformBoardgameItemToDbFormat: (item: BoardGameItem) => NewBoardGameItemModel = (item: BoardGameItem) => {
+    return {
+        objectId: item.objectId,
+        image: item.image,
+        name: item.name,
+        thumbnail: item.thumbnail,
+        yearPublished: item.yearPublished,
+        createdAt: String(new Date()),
+        updatedAt: String(new Date()),
+        stats: {
+            minPlayers: item.stats.minPlayers,
+            maxPlayers: item.stats.maxPlayers,
+            minPlayTime: item.stats.minPlayTime,
+            maxPlayTime: item.stats.maxPlayTime,
+            playingTime: item.stats.playingTime,
+            playersWhoOwnThisGame: item.stats.playersWhoOwnThisGame,
+        },
+        ratingStats: {
+            usersRated: item.ratingStats.usersRated,
+            average: item.ratingStats.average,
+            bayesAverage: item.ratingStats.bayesAverage,
+            stdDeviation: item.ratingStats.stdDeviation,
+            median: item.ratingStats.median,
+        },
+    };
+};
+
 export const loadItemsFromCollectionIntoDb = async (username: string) => {
-    const collectionXml = await getCollectionFromBGG(username);
-    const boardgameObject: any = {};
-
-    const jsonCollection = parser.parse(collectionXml, xmlOptions) as any;
-
-    if (!!jsonCollection?.errors?.error) {
-        console.log('\x1b[44m%s \x1b[0m', '[matt] HERE 3 ERRPR ');
-
-        throw new Error(jsonCollection?.errors?.error?.message);
-    }
-
-    if (jsonCollection && !!jsonCollection?.message) {
-        console.log('\x1b[44m%s \x1b[0m', '[matt] MESSAGE');
-
-        // [matt] Need to do retries for this. Maybe a redis job??
-        return new Error('They are timing us out');
-    }
+    const fullCollection = await getCollectionFromBGG(username);
 
     // [matt] Remove the count when closer to being done, we want ALL the items in the db
     let count = 0;
 
     try {
-        for (const item of jsonCollection.items.item) {
+        for (const item of fullCollection.items.item) {
             if (count < 31) {
                 const parsedItem = parseBoardgameItem(item);
-                console.log('\x1b[42m%s \x1b[0m', '[matt] item', item.stats.rating);
-                const newBoardGameItemForDb: NewBoardGameItemModel = {
-                    objectId: parsedItem.objectId,
-                    image: parsedItem.image,
-                    name: parsedItem.name,
-                    thumbnail: parsedItem.thumbnail,
-                    yearPublished: parsedItem.yearPublished,
-                    createdAt: String(new Date()),
-                    updatedAt: String(new Date()),
-                    stats: {
-                        minPlayers: parsedItem.stats.minPlayers,
-                        maxPlayers: parsedItem.stats.maxPlayers,
-                        minPlayTime: parsedItem.stats.minPlayTime,
-                        maxPlayTime: parsedItem.stats.maxPlayTime,
-                        playingTime: parsedItem.stats.playingTime,
-                        playersWhoOwnThisGame: parsedItem.stats.playersWhoOwnThisGame,
-                    },
-                    ratingStats: {
-                        usersRated: parsedItem.ratingStats.usersRated,
-                        average: parsedItem.ratingStats.average,
-                        bayesAverage: parsedItem.ratingStats.bayesAverage,
-                        stdDeviation: parsedItem.ratingStats.stdDeviation,
-                        median: parsedItem.ratingStats.median,
-                    },
-                };
+                const thing = await getSingleBoardGameFromBGG([parsedItem.objectId]);
+
+                console.log('\x1b[41m%s \x1b[0m', '[matt] thing', thing);
+                // console.log('\x1b[42m%s \x1b[0m', '[matt] item', item);
+                const newBoardGameItemForDb: NewBoardGameItemModel = transformBoardgameItemToDbFormat(parsedItem);
 
                 collectionRepo.upsertBoardGameItem(newBoardGameItemForDb);
-
-                boardgameObject[parsedItem.objectId] = { ...parsedItem };
             }
             count = count + 1;
         }
     } catch (err) {
-        console.log(jsonCollection, err);
+        console.log(fullCollection, err);
         return new Error(err);
     }
 
-    return { message: `${jsonCollection.items.item.length} records added to the DB` };
+    return { message: `${fullCollection.items.item.length} records added to the DB` };
 };
